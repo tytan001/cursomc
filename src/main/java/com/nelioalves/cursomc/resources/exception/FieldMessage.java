4d00b67ab0ae6867f8bd9442f3a21c24e201478f
Yan silva <yancarlosdasilva@hotmail.com>
2018-12-10 18:17:14 -0200

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/nelioalves/cursomc/resources/exception/FieldMessage.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/FieldMessage.java
@@ -6,15 +6,15 @@ public class FieldMessage implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private String fieldName;
-	private String massage;
+	private String message;
 	
 	public FieldMessage() {
 	}
 
-	public FieldMessage(String fieldName, String massage) {
+	public FieldMessage(String fieldName, String message) {
 		super();
 		this.fieldName = fieldName;
-		this.massage = massage;
+		this.message = message;
 	}
 
 	public String getFieldName() {
@@ -25,12 +25,12 @@ public class FieldMessage implements Serializable{
 		this.fieldName = fieldName;
 	}
 
-	public String getMassage() {
-		return massage;
+	public String getMessage() {
+		return message;
 	}
 
-	public void setMassage(String massage) {
-		this.massage = massage;
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
 }
